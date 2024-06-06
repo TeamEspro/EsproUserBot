9263e47c1624eb1f6300bc8587d9d952a00464df
TeamEspro <teamespro09@gmail.com>
2024-06-06 18:31:26 +0530

    EsproUserBot

---
+++ EsproUser/modules/helpers/inline.py
@@ -0,0 +1,86 @@
+import asyncio
+
+from ... import *
+from .buttons import *
+from .wrapper import *
+from pyrogram.types import *
+
+
+async def help_menu_logo(answer):
+    image = None
+    if image:
+        thumb_image = image
+    else:
+        thumb_image = "https://telegra.ph/file/df1e7c4955bd0e05e34cd.jpg"
+    button = paginate_plugins(0, plugs, "help")
+    answer.append(
+        InlineQueryResultPhoto(
+            photo_url=f"{thumb_image}",
+            title="🥀 Help Menu ✨",
+            thumb_url=f"{thumb_image}",
+            description=f"🥀 Open Help Menu Of Espro-Userbot ✨...",
+            caption=f"""
+**🥀 Welcome To Help Menu Of
+Espro Userbot » {__version__} ✨...
+
+Click On Below 🌺 Buttons To
+Get Userbot Commands.
+
+🌷Powered By : [Espro Support](https://t.me/EsproSupport).**
+            """,
+            reply_markup=InlineKeyboardMarkup(button),
+        )
+    )
+    return answer
+
+
+async def help_menu_text(answer):
+    from ... import __version__
+    button = paginate_plugins(0, plugs, "help")
+    answer.append(
+        InlineQueryResultArticle(
+            title="🥀 Help Menu ✨",
+            input_message_content=InputTextMessageContent(f"""
+**🥀 Welcome To Help Menu Of
+Espro Userbot » {__version__} ✨...
+
+Click On Below 🌺 Buttons To
+Get Userbot Commands.
+
+🌷Powered By : [Espro Support](https://t.me/EsproSupport).**""",
+            disable_web_page_preview=True
+            ),
+            reply_markup=InlineKeyboardMarkup(button),
+        )
+    )
+    return answer
+
+
+async def run_async_inline():
+    @bot.on_inline_query()
+    @inline_wrapper
+    async def inline_query_handler(bot, query):
+        text = query.query
+        if text.startswith("help_menu_logo"):
+            answer = []
+            answer = await help_menu_logo(answer)
+            try:
+                await bot.answer_inline_query(
+                    query.id, results=answer, cache_time=10
+                )
+            except Exception as e:
+                print(str(e))
+                return
+        elif text.startswith("help_menu_text"):
+            answer = []
+            answer = await help_menu_text(answer)
+            try:
+                await bot.answer_inline_query(
+                    query.id, results=answer, cache_time=10
+                )
+            except Exception as e:
+                print(str(e))
+                return
+        else:
+            return
+
---
+++ EsproUser/modules/helpers/wrapper.py
@@ -0,0 +1,114 @@
+from pyrogram.types import *
+from traceback import format_exc
+
+from ...console import SUDOERS
+from ..clients.clients import app, bot
+
+def super_user_only(mystic):
+    async def wrapper(client, message):
+        try:
+            if message.from_user.is_self:
+                return await mystic(client, message)
+        except:
+            if message.outgoing:
+                return await mystic(client, message)
+            
+    return wrapper
+
+
+
+def sudo_users_only(mystic):
+    async def wrapper(client, message):
+        try:
+            if (message.from_user.is_self or
+               message.from_user.id in SUDOERS
+            ):
+                return await mystic(client, message)
+        except:
+            if (message.outgoing or
+               message.from_user.id in SUDOERS
+            ):
+                return await mystic(client, message)
+            
+    return wrapper
+    
+
+def cb_wrapper(func):
+    async def wrapper(bot, cb):
+        sudousers = SUDOERS
+        if (cb.from_user.id != app.me.id and
+            cb.from_user.id not in sudousers
+        ):
+            return await cb.answer(
+                "❎ You Are Not A Sudo User❗",
+                cache_time=0,
+                show_alert=True,
+            )
+        else:
+            try:
+                return await func(bot, cb)
+            except Exception:
+                print(format_exc())
+                return await cb.answer(
+                    f"❎ Something Went Wrong, Please Check Logs❗..."
+                )
+        
+    return wrapper
+
+
+def inline_wrapper(func):
+    from ... import __version__
+    async def wrapper(bot, query):
+        sudousers = SUDOERS
+        if (query.from_user.id != app.me.id and
+            query.from_user.id not in sudousers
+        ):
+            try:
+                button = [
+                    [
+                        InlineKeyboardButton(
+                            "💥 Deploy Espro Userbot ✨",
+                            url=f"https://github.com/TeamEspro/EsproUserBot"
+                        )
+                    ]
+                ]
+                await bot.answer_inline_query(
+                    query.id,
+                    cache_time=1,
+                    results=[
+                        (
+                            InlineQueryResultPhoto(
+                                photo_url=f"https://telegra.ph/file/e40bad038936f01b11317.jpg",
+                                title="🥀 Espro Userbot ✨",
+                                thumb_url=f"https://telegra.ph/file/e40bad038936f01b11317.jpg",
+                                description=f"🌷 Deploy Your Own Espro-Userbot 🌿...",
+                                caption=f"<b>🥀 Welcome » To » Espro 🌷\n✅ Userbot {__version__} ✨...</b>",
+                                reply_markup=InlineKeyboardMarkup(button),
+                            )
+                        )
+                    ],
+                )
+            except Exception as e:
+                print(str(e))
+                await bot.answer_inline_query(
+                    query.id,
+                    cache_time=1,
+                    results=[
+                        (
+                            InlineQueryResultArticle(
+                                title="",
+                                input_message_content=InputTextMessageContent(
+                                    f"||**🥀 Please, Deploy Your Own Espro Userbot❗...\n\nRepo:** <i>https://github.com/TeamEspro/EsproUserBot/</i>||"
+                                ),
+                            )
+                        )
+                    ],
+                )
+            except Exception as e:
+                print(str(e))
+                pass
+        else:
+           return await func(bot, query)
+
+    return wrapper
+
