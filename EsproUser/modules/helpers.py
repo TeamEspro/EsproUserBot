ea60204e375e882f1bb6d44e5d588bae81fe746f
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-03-11 05:18:16 +0530

    Update helpers.py

--- EsproUser/modules/helpers.py
+++ EsproUser/modules/helpers.py
@@ -1,8 +1,10 @@
 import asyncio, os, yt_dlp
 
+COOKIE_FILE = "cookies.txt"  # Cookies file ka path
 
 async def download_media_file(link: str, type: str):
     loop = asyncio.get_running_loop()
+    
     if type == "Audio":
         ydl_opts = {
             "format": "bestaudio/best",
@@ -11,8 +13,8 @@ async def download_media_file(link: str, type: str):
             "nocheckcertificate": True,
             "quiet": True,
             "no_warnings": True,
+            "cookies": COOKIE_FILE,  # Cookies ka use
         }
-
     elif type == "Video":
         ydl_opts = {
             "format": "(bestvideo[height<=?720][width<=?1280][ext=mp4])+(bestaudio[ext=m4a])",
@@ -21,14 +23,16 @@ async def download_media_file(link: str, type: str):
             "nocheckcertificate": True,
             "quiet": True,
             "no_warnings": True,
+            "cookies": COOKIE_FILE,  # Cookies ka use
         }
-        
+    
     x = yt_dlp.YoutubeDL(ydl_opts)
     info = x.extract_info(link, False)
-    file = os.path.join(
-        "downloads", f"{info['id']}.{info['ext']}"
-    )
+    
+    file = os.path.join("downloads", f"{info['id']}.{info['ext']}")
+    
     if os.path.exists(file):
         return file
+    
     await loop.run_in_executor(None, x.download, [link])
     return file
